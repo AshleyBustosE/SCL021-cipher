e8a545e9ae65dcfc806e8df09462e86f66950055
AshleyBustos <ashley.bustos.e@gmail.com>
2022-07-03 16:26:16 -0400

    agregamos index js y css

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,13 @@
-const cipher = {
+//const cipher = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
   // ...
 };
 
+//Asignaremos un array 
+var abecedario = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] 
+
+console.log(abecedario.lenght)
+
+
 export default cipher;
+
+alert("Hola") 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+let Boton1 = document.getElementById("Boton1");
+let Boton2 = document.getElementById("Boton2");
+
+Boton1.addEventListener("click", codificar);
+Boton2.addEventListener("click", decodificar);
+
+function codificar(){
+    alert("¿Quieres codificar?");
+}
+function decodificar(){
+    alert("¿Quieres decodificar?"); 
+}
+
+//alert("Chao nomas");
