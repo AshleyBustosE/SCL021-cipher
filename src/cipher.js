200edc062e8a4f9f90f5e6fe1ef628dd59793fe2
AshleyBustos <ashley.bustos.e@gmail.com>
2022-07-03 17:02:21 -0400

    agregamos nuevo js

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,6 @@
 //const cipher = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
   // ...
-};
+//};
 
 //Asignaremos un array 
 var abecedario = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] 
