296f6cf81a58b42468fb684e1f942f1b225d484f
AshleyBustos <ashley.bustos.e@gmail.com>
2022-07-18 22:43:56 -0400

    Código pasó los test y está hecho el readme

--- src/cipher.js
+++ src/cipher.js
@@ -1,78 +1,53 @@
-const cipher = { 
-  encode: function (desplazamiento, texto){
-    //let asciiCambiado = 0;
+const cipher = {
+  encode: function (desplazamiento, texto) {
+    if (!texto) {
+      throw new TypeError();
+    }
+    if (!desplazamiento) {
+      throw new TypeError();
+    }
+
     let resultado = "";
-    //let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    desplazamiento = (desplazamiento % 26 + 26) % 26;
+    desplazamiento = ((desplazamiento % 26) + 26) % 26;
 
-    //if (texto){
     for (let i = 0; i < texto.length; i++) {
       let ascii = texto.charCodeAt(i);
       if (ascii >= 65 && ascii <= 90) {
-        let asciiCambiado = (ascii - 65 + desplazamiento) % 26 + 65;
+        let asciiCambiado = ((ascii - 65 + desplazamiento) % 26) + 65;
         resultado = resultado + String.fromCharCode(asciiCambiado);
-      }
-
-      /*else if (ascii >= 48 && ascii <= 57) {
-        asciiCambiado = (((ascii - 48 + desplazamiento) % 10) + 48);
-        resultado += String.fromCharCode(asciiCambiado);
-      }*/
-
-      else {
+      } else {
         resultado = resultado + String.fromCharCode(ascii);
       }
-
-      //console.log(ascii)
-        //if (letras)
     }
 
     return resultado;
   },
-    /*if (!texto)
-      return "";
-    const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    desplazamiento = (desplazamiento % 26 + 26) % 26;
-    return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)+desplazamiento)%26]);
-  },*/
 
-  decode: function (desplazamiento, texto){
-    //let asciiCambiado = 0;
+  decode: function (desplazamiento, texto) {
+    if (!texto) {
+      throw new TypeError();
+    }
+    if (!desplazamiento) {
+      throw new TypeError();
+    }
+
     let resultado = "";
- 
-    desplazamiento = (desplazamiento % 26 - 26) % 26;
 
-    //if (texto){
+    desplazamiento = ((desplazamiento % 26) - 26) % 26;
+
     for (let i = 0; i < texto.length; i++) {
       let ascii = texto.charCodeAt(i);
       if (ascii >= 65 && ascii <= 90) {
-        let asciiCambiado = (ascii - 65 - desplazamiento) % 26 + 65;
+        let asciiCambiado = ((ascii - 65 - desplazamiento) % 26) + 65;
         resultado = resultado + String.fromCharCode(asciiCambiado);
-      }
-
-      /*else if (ascii >= 48 && ascii <= 57) {
-        asciiCambiado = (((ascii - 57 - desplazamiento) % 10) + 57);
-        resultado += String.fromCharCode(asciiCambiado);
-      }*/
-
-      else {
+      } else {
         resultado = resultado + String.fromCharCode(ascii);
       }
-      //console.log(ascii)
-        //if (letras)
     }
 
     return resultado;
-
-    }
-  }
-//}
-    /*if (!texto)
-      return "";
-    const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    desplazamiento = (desplazamiento % 26 - 26) % 26;
-    return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)-desplazamiento)%26]);
-  } */
-
+  },
+};
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,46 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
 //Saludamos
-alert("Bienvenido, ")
+alert("Hola");
+alert("Puedes escribir un mensaje secreto y compartirlo con tu entorno");
 
 // Para que se comience a ejecutar la página
 window.addEventListener("load", inicio, true);
 
 // Una vez iniciada, da comienzo a sus funciones
-function inicio(){
-     document.getElementById("mensaje").addEventListener("keyup", function(){               // Llamamos a "mensaje" en HTML para que una vez que se pulse la tecla se aplique mayúscula
-        this.value = this.value.toUpperCase();
-        }, true);
+function inicio() {
+  document.getElementById("mensaje").addEventListener(
+    "keyup",
+    function () {
+      this.value = this.value.toUpperCase(); // Llamamos a "mensaje" en HTML para que una vez que se pulse la tecla se aplique mayúscula
+    },
+    true
+  );
 
-     document.getElementById("cifrar").addEventListener("click", function(){                // Se llama al botón "cifrar" en HTML para que una vez que se haga click ejecute su función
-        let texto = document.getElementById("mensaje").value;                               // Se aplican variables let de los dos datos ingresados; mensaje y desplazamiento junto a value para darles valor
-        let desplazamiento = document.getElementById("desplazamiento").value;
-        document.getElementById("mensaje2").value = cipher.encode(desplazamiento, texto);
-        /*let mensaje2 = document.getElementById("mensaje2").value;                         // Para que muestre error cuando ingresan letras o signos en el mensaje
-        if (texto !== null && texto !== '' && texto !== undefined) {
-            alert ("Por favor ingrese sólo letras")
-        } */
-    }, true);
-     document.getElementById("descifrar").addEventListener("click", function(){
-        let texto = document.getElementById("mensaje").value;
-        let desplazamiento = document.getElementById("desplazamiento").value;
-        document.getElementById("mensaje2").value = cipher.decode(desplazamiento, texto);
-        }, true);
+  // Se llama al botón "cifrar" en HTML para que una vez que se haga click ejecute su función
+  document.getElementById("cifrar").addEventListener(
+    "click",
+    function () {
+      let texto = document.getElementById("mensaje").value; // Se aplican variables let de los dos datos ingresados; mensaje y desplazamiento junto a value para darles valor
+      let desplazamiento = document.getElementById("desplazamiento").value;
+      document.getElementById("mensaje2").value = cipher.encode(
+        desplazamiento,
+        texto
+      );
+    },
+    true
+  );
+
+  document.getElementById("descifrar").addEventListener(
+    "click",
+    function () {
+      let texto = document.getElementById("mensaje").value;
+      let desplazamiento = document.getElementById("desplazamiento").value;
+      document.getElementById("mensaje2").value = cipher.decode(
+        desplazamiento,
+        texto
+      );
+    },
+    true
+  );
 }
-
-
-//console.log(cipher);
-
